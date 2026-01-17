c4a55184f2559f1b21b1a440a2b1bcccd8c8c958
chetan-bist <bistc3712@gmail.com>
2026-01-17 11:52:28 +0545

    feat: update App and main entry to support dynamic components

---
+++ src/component/dynamic-component.jsx
@@ -0,0 +1,10 @@
+function DynamicComponent() {
+    let name = "chetabist";
+    let age = "23";
+    return(
+        <div>
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+        </div>
+    )
+}export default DynamicComponent;
